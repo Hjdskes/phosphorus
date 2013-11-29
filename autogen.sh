69b4733ae974a5d322f04e9aaf081409f2341b18
Unia <jthidskes@outlook.com>
2013-11-29 15:31:03 +0100

    revert po mess, figure that out another time

---
+++ autogen.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Regenerating autotools files"
+autoreconf --force --install || exit 1
+
+#echo "Setting up Intltool"
+#intltoolize --copy --force --automake || exit 1
+
+echo
+echo "You are now ready to run ./configure"
